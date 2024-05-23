d1fff410fff85f7779265a63648d45c3c305f081
Abhishek Kumar <abhishekumar1772004@gmail.com>
2024-05-23 16:15:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// ******** Auto typing **********
+
+var typed = new Typed(".auto-type",{
+    strings : ["Web Developer","Prompt Engineer","AI Chatbot Developer","Video Editor"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop : true,
+    
+})
+
+// ************ Navbar hamberger button.. *****************
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const headerElem = document.querySelector(".header");
+
+mobile_nav.addEventListener("click", () => {
+    headerElem.classList.toggle("active");
+});
